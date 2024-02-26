9746a0d2832046cf361541a69de72b5dcd52a50f
Maomiaos <1444645996@qq.com>
2024-02-26 19:48:30 +0800

    first springboot

---
+++ src/main/java/com/ymx_project/YmxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ymx_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmxProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmxProjectApplication.class, args);
+    }
+
+}
